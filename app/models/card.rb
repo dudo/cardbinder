240a58251857c49ee3c0a5d721fda41caedc049c
Brett C. Dudo <brett.dudo@gmail.com>
2016-07-11 00:28:31 -0700

    different sort

--- app/models/card.rb
+++ app/models/card.rb
@@ -34,7 +34,7 @@ class Card
 
   # this is used for the header menu, and almost always available
   def self.sets
-    CardSet.not_in(code: CardSet::NON_STANDARD_SETS + CardSet::DONT_HAVE_PICS).order_by([[:releaseDate, :asc]])
+    CardSet.where(type: %w(core expansion)).not_in(CardSet::DONT_HAVE_PICS).order_by([[:releaseDate, :asc]])
   end
 
   def self.aliases(a)
--- app/models/card_set.rb
+++ app/models/card_set.rb
@@ -15,14 +15,6 @@ class CardSet
   index(code: 1)
   index(releaseDate: 1)
 
-  NON_STANDARD_SETS = %w(CED CEI pDRC pLGM pMEI MGB pMGD pJGP pPRE PTK pARL S00 DKM
-                         pALP VAN pWOR PO2 ATH POR CHR pPOD ITP RQS pGRU pCEL UGL
-                         CEI CED ME2 pWPN DD2 DDC V09 HOP ME3 DDD H09 DDE DPA ARC
-                         V10 DDF PD2 ME4 DDG CMD V11 DDH PD3 DDI PC2 V12 DDJ CM1
-                         DDK pWCQ MMA V13 DDL C13 DDM MD1 CPK CNS VMA V14 DDN DD3_DVD DD3_EVG
-                         C14 S99 DD3_GVL DD3_JVC FRF_UGIN DDO pWOS TPR MM2 DDP
-                         V15 C15 W16 DDQ EMA EVG p15A BRB DRB pLPA pSUM MED pGPX pHHO pPRO
-                         CST pCMP UNH pGTW p2HG pSUS pREL pFNM pMPR BTD pELP)
   DONT_HAVE_PICS = %w(M15 KTK FRF DTK BFZ OGW EXP ORI SOI EMN)
 
 end
